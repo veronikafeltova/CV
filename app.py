1e104e2b3f6b9619153283e13cc5ee13f8180cc0
Veronika Feltová <veronika.feltova@gmail.com>
2023-05-24 20:44:05 +0200

    zmena

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ profile_pic = current_dir / "assets" / "profile-pic.png"
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "Digital CV | John Doe"
 PAGE_ICON = ":wave:"
-NAME = "John Doe"
+NAME = "Veronika Feltová"
 DESCRIPTION = """
 Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
 """
